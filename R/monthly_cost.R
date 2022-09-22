c224ab906b18d961782f9c1b62c251a9dc4f53ee
LittleBeannie <yujie.zhao@merck.com>
2022-09-22 00:14:19 -0400

    update

--- R/monthly_cost.R
+++ R/monthly_cost.R
@@ -1,64 +1,53 @@
-############ part 2： 小小函数again，用来算每月成本 ###########
-#' Title
+#' Calculate monthly cost of a house
 #'
-#' @param house_value 房屋价格
-#' @param property_tax 每年地税率
-#' @param other_tax 每年其他税率
-#' @param one_time_fee 一次性费用
-#' @param annual_other_tax 其他税：school/county tax等等
-#' @param monthly_hoa_fee 每月物业费
-#' @param monthly_home_insurance 每月房屋保险费
-#' @param monthly_utility_fee 每月水电网支出
-#' @param closing_fee 一次性过户费用，点数等等
-#' @param year 房子投资年数
-#' @param saving_rate 定期年利息（首付款的潜在机会成本）
-#' @param down_payment 首付总额（万）
-#' @param mortgage_rate 贷款年利率
-#' @param morrgage_year 贷款年份
+#' @param house_value house value
+#' @param property_tax_rate annual property tax rate
+#' @param other_tax annual other tax rate
+#' @param one_time_fee one time fee, e.g., agent fee, points fee
+#' @param annual_other_tax other tex fee, e.g., school/county tax
+#' @param monthly_hoa_fee monthly HOA fee
+#' @param monthly_home_insurance monthly home insurance fee
+#' @param monthly_utility_fee monthly utility fee
+#' @param closing_fee closing fee
+#' @param year number of years for invest this house 
+#' @param saving_rate saving rate, which is potential loss of the down payment
+#' @param down_payment_rate down payment
+#' @param mortgage_rate annual mortgage rate
+#' @param morrgage_year mortgage years
 #'
 #' @return
 #' @export
 #'
 #' @examples
 monthly_cost <- function(
-  # 房屋价格
   house_value = 20*1e4, 
-  # tax支出
-  property_tax = 1.54/100,        # 每年地税率
+  property_tax_rate = 1.54/100,        
   other_tax = 1/100,
   one_time_fee = 1e3,
-  annual_other_tax = 3000,        # 其他税：school/county tax等等
-  # 每月花费
-  monthly_hoa_fee = 150,          # 每月物业费
-  monthly_home_insurance = 150,   # 每月房屋保险
-  monthly_utility_fee = 300,      # 每月水电网支出
-  # 一次性费用
-  closing_fee,                    # 中介费(卖出时), 过户费, 点数，等等
-  # 房子投资年数
+  annual_other_tax = 3000,        
+  monthly_hoa_fee = 150,          
+  monthly_home_insurance = 150,   
+  monthly_utility_fee = 300,      
+  closing_fee,                 
   year = 5,
-  # 定期年利息（首付款的潜在机会成本）
   saving_rate = 0.03,  
-  # 首付总额（万）
-  down_payment = 0.2,   
-  # 贷款年利率
+  down_payment_rate = 0.2,   
   mortgage_rate = 0.0375,
-  morrgage_year = 30             # 贷款年份
+  morrgage_year = 30             
 ){
-  # 每年要交的地税
-  annual_property_tax = house_value * property_tax   
   
-  # 每年需要的费用：物业费, 地税，等等
+  # the annual cost: HOA + property and other tax fee + home insurance + utility
   annual_fee = monthly_hoa_fee * 12 + 
-    property_tax + other_tax + 
+    house_value * property_tax_rate + other_tax + 
     monthly_home_insurance * 12 + 
     monthly_utility_fee * 12 
   
-  # 贷款总额
-  mortgage = house_value * (1 - down_payment)                    
-  monthly_mortgage <- repayment(mortgage,               # 贷款额
-                                mortgage_rate / 12,     # 年利率/12
-                                morrgage_year * 12      # 贷款年数*12
+  # total mortgage amount
+  mortgage = house_value * (1 - down_payment_rate)                    
+  monthly_mortgage <- repayment(mortgage,               
+                                mortgage_rate / 12,     
+                                morrgage_year * 12      
   ) 
   
-  return(one_time_fee/year/12 + annual_fee/12 + (down_payment + one_time_fee)*saving_rate/12 + monthly_mortgage)
+  return(one_time_fee/year/12 + annual_fee/12 + (house_value*down_payment_rate  + one_time_fee)*saving_rate/12 + monthly_mortgage)
 }
--- R/repayment.R
+++ R/repayment.R
@@ -1,38 +1,28 @@
-#' 小小函数，可以直接忽略，已经检验过算法没问题哈哈哈
-#' 计算每月的贷款
-#'
-#' @param A 贷款本金
-#' @param β 月利息
-#' @param m 期数（月）
+#' Calculate the monthly mortgage
+#' 
+#' @param A loan principal
+#' @param beta monthly loan rate
+#' @param m number of periods in months
 #'
 #' @return
 #' @export
 #'
 #' @examples
-repayment <- function(A, β, m){                # 贷款本金、月利息、期数（月）
-  j = (1:m)                                    # 定义向量，用于中间过程计算
-  X = A*β*(1+β)^(m)/((1+β)^(m)-1)              # 每期应还(本+息),见推导公式①
+repayment <- function(A, beta, m){             
+  j = (1:m)                                    
+  X = A*beta*(1+beta)^(m)/((1+beta)^(m)-1)     
   I = c()
-  I[j] = A*β*(1+β)^(j-1) - X*((1+β)^(j-1)-1)   # 每期应还利息,见推导公式②
-  c = c()                                      # 定义一个空向量用于计算等额偿还本金，否则会报错
-  c = X - I                                    # 每期应还本金
+  I[j] = A*beta*(1+beta)^(j-1) - X*((1+beta)^(j-1)-1)   
+  c = c()   
+  c = X - I       
   a = c()
-  a[j] = A*(1+β)^j - ((1+β)^j-1)/(β) * X       # 每期末本金余额,见推导公式①           
+  a[j] = A*(1+beta)^j - ((1+beta)^j-1)/(beta) * X            
   
-  ## 打印输出总利息，总本金
-  # print(paste0('总利息：',round(sum(I),2)))
-  # print(paste0('总本金：',round(sum(c),2)))
+  col1 = round(c,2)                             
+  col2 = round(I,2)                             
+  col3 = round(a,2)                            
+  col4 = round(X,2)                          
   
-  ## 将等额本息还款数据输出至本地
-  col1 = round(c,2)                             # 每期应还本金
-  col2 = round(I,2)                             # 每期应还利息
-  col3 = round(a,2)                             # 每期末本金余额
-  col4 = round(X,2)                             # 每期应还
-  
-  #loan = data.frame(期数 = c(1:m), 
-  #                  每期应还本金 = col1, 
-  #                  每期应还利息 = col2, 
-  #                  每期末本金余额 = col3, 
-  #                  每期应还 = col4)
+
   return(unique(col4))
 }
